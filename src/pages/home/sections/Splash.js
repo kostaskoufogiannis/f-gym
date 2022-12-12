27e783e6c2debb56f31ba2cf5ebbd41cf99018e4
Kostas Koufogiannis <kostaskoufogiannis@gmail.com>
2022-12-12 23:18:01 +0200

    final

--- src/pages/home/sections/Splash.js
+++ src/pages/home/sections/Splash.js
@@ -1,20 +1,25 @@
 import ScrollDown from "../ScrollDown";
+// import Img from "../../../assets/home_bg.png";
 
 const Splash = () => {
   return (
     <div className="relative flex h-full items-center">
       <div>
-        <div className="mb-5 max-w-4xl pl-2 text-5xl font-bold md:text-8xl">
+        <div className="mb-5 max-w-5xl pl-2 text-5xl font-bold lg:text-7xl xl:text-9xl">
           <div className="text-white">
-            You can have <br />
-            <span className="fgym-underline">results</span>
-            <span>
-              {" "}
-              or excuses <br /> not both.
-            </span>
+            Don't expect to
+            <br />
+            see a <span className="fgym-underline">change</span>
+            <br />
+            if you don't <br /> make one.
           </div>
         </div>
       </div>
+      {/* <img
+        src={Img}
+        alt=""
+        className="absolute right-0 top-1/2 h-[70vh] -translate-y-1/2 transform"
+      /> */}
       <ScrollDown />
     </div>
   );
